62640ee01945767ac69c404d4fa1512b9924fb77
Brian Pilati <brian@homie.com>
2018-07-13 14:23:01 -0600

    More commits for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -11,22 +12,22 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class AppComponent {
   title = 'app';
 
-  users: Observable<any[]>;
+  songs: Observable<any[]>;
   user: any;
   constructor(db: AngularFirestore, afAuth: AngularFireAuth) {
-    this.users = db.collection('users').valueChanges();
-    this.user = {};
 
-    console.log(this.users);
-
+    /*
     afAuth.auth.createUserWithEmailAndPassword('brian1@gmail.com', '12345678').then(res => {
       console.log('there', res);
     }, error => {
       console.log('here');
     }
     );
+    */
 
+    this.user = {};
 
+    // 4s2DhhKj6CfxWwdfZ7MfDrSkyyt1
     afAuth.auth.signInWithEmailAndPassword('brianpilati@gmail.com', '12345678').then( res => {
       console.log('sign in 1', res);
       console.log('sign in 2', res.user.uid);
@@ -37,18 +38,43 @@ export class AppComponent {
 
       console.log('is this old?');
       db.doc('users/' + res.user.uid).valueChanges().subscribe(_user_ => {
+      // db.doc('users/222').valueChanges().subscribe(_user_ => {
         this.user = _user_;
-        console.log(_user_);
-        console.log(_user_['firstName']);
-        console.log(this.user, this.user.firstName);
+        console.log(1, _user_);
+        console.log(2, _user_['firstName']);
+        console.log(3, this.user, this.user.firstName);
       });
-      /*
-      db.collection('users').doc(res.user.uid).set(user).then(resuser => {
+
+      // db.collection('users').doc(res.user.uid).set(user).then(resuser => {
+      // db.collection('users').doc('2222222').set(user).then(resuser => {
+        /*
+      db.collection('users').doc('444444').set(user).then(resuser => {
         console.log('added', resuser);
       }, error => {
         console.log('error', error);
       });
       */
+
+      /*
+      db.collection('songs').valueChanges().subscribe(_songs_ => {
+        this.songs = _songs_;
+        console.log(_songs_);
+      });
+      */
+
+      this.songs = db.collection('songs').snapshotChanges().pipe(
+        map(actions => {
+          return actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            console.log(Object.assign({ uid: id}, data));
+            return Object.assign({ uid: id}, data);
+            //  return { id, data};
+          });
+        })
+      );
+
+      console.log(1, this.songs);
 
     }, error => {
       console.log('sign in error');
