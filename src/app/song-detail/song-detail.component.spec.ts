d5e53bc2b23322a225d85859430be986f87f48ca
Brian Pilati <brianpilati@gmail.com>
2018-08-02 13:14:36 -0600

    Two styles of testing async calls to keep the skills sharp

--- src/app/song-detail/song-detail.component.spec.ts
+++ src/app/song-detail/song-detail.component.spec.ts
@@ -1,9 +1,21 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { tick, async, ComponentFixture, TestBed, inject, fakeAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from '../../testing/in-memory-data.service';
 
 import { SongDetailComponent } from './song-detail.component';
 import { ActivatedRoute } from '../../../node_modules/@angular/router';
+
+const activatedRouteMock =  {
+  snapshot: {
+    paramMap: {
+      get: function() {
+        return 1;
+      }
+    }
+  }
+};
 
 describe('SongDetailComponent', () => {
   let component: SongDetailComponent;
@@ -11,37 +23,31 @@ describe('SongDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [
+        HttpClientTestingModule,
+         RouterTestingModule
+      ],
       declarations: [ SongDetailComponent ],
       providers: [ 
         { 
           provide: ActivatedRoute, 
-          useValue: {
-            snapshot: {
-              paramMap: {
-                get: function() {
-                  return 1;
-                }
-              }
-            }
-          } 
+          useValue: activatedRouteMock 
         } 
       ]
     })
-    .compileComponents();
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(SongDetailComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SongDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should test getSong', inject([HttpTestingController],
+  it('should test getSong', fakeAsync(inject([HttpTestingController],
     (httpMock: HttpTestingController) => {
       expect(component.song).toEqual({});
 
@@ -54,9 +60,56 @@ describe('SongDetailComponent', () => {
       });
 
       expect(component.song).toEqual({ id: 1, title: 'Africa', artist: 'Toto' });
-  }));
+  })));
 
   it('should test getSongImage',() => {
       expect(component.getSongImage()).toBe('assets/africa.png');
   });
+});
+
+describe('SongDetailComponent with Fake Data', () => {
+  let component: SongDetailComponent;
+  let fixture: ComponentFixture<SongDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        HttpClientInMemoryWebApiModule.forRoot(
+          InMemoryDataService, { 
+            dataEncapsulation: false,
+            delay: 1500 
+           }
+        ),
+         RouterTestingModule
+      ],
+      declarations: [ SongDetailComponent ],
+      providers: [ 
+        { 
+          provide: ActivatedRoute, 
+          useValue: activatedRouteMock 
+        } 
+      ]
+    })
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(SongDetailComponent);
+      component = fixture.componentInstance;
+    });
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should test getSong whenStable', fakeAsync(() => {
+    expect(component.song).toEqual({});
+
+    fixture.detectChanges();
+    tick(1501);
+
+    fixture.whenStable().then(() => {
+      expect(component.song).toEqual({ id: 1, title: 'Africa', artist: 'Toto' });
+    });
+  }));
 });
