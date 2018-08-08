87e1e5c312a09fe5b9ddafa7e5bfb8363113f686
Brian Pilati <brianpilati@gmail.com>
2018-08-08 17:41:23 -0600

    Now work on the song file builder

--- node/utils/song_file_builder.js
+++ node/utils/song_file_builder.js
@@ -1,14 +1,19 @@
 var fs = require('fs');
 var pool = require('../lib/database');
+var SongDomain = require('../server/domains/song');
+const songDomain = new SongDomain(pool);
 
-pool.query('SELECT * FROM songs').then(result => {
-    result.forEach(song=> {
-        fs.writeFile(`/tmp/test_${song.title}`, err => {
-            if(err) {
-                return console.log(err);
-            }
-            console.log(`The ${song.title} file was saved!`);
-        }); 
+songDomain.getSongs().then(result => {
+  result.forEach(song => {
+    console.log(2, song.title);
+    fs.writeFile(`../../deployment/test_${song.title}.html`, song.title, err => {
+      console.log('here');
+      if (err) {
+        console.log(222);
+        return console.log(err);
+      }
+      console.log(`The ${song.title} file was saved!`);
     });
-    pool.end();
+  });
+  pool.end();
 });
