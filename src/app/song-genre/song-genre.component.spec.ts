f36a7d45dbadfdceec3248413105a44276595629
Brian Pilati <brianpilati@gmail.com>
2018-08-08 11:27:36 -0600

    Added testing for song-genre

--- src/app/song-genre/song-genre.component.spec.ts
+++ src/app/song-genre/song-genre.component.spec.ts
@@ -1,24 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SongGenreComponent } from './song-genre.component';
+import { MatIconModule } from '../../../node_modules/@angular/material';
 
 describe('SongGenreComponent', () => {
   let component: SongGenreComponent;
   let fixture: ComponentFixture<SongGenreComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatIconModule],
       declarations: [SongGenreComponent]
-    }).compileComponents();
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(SongGenreComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        compiled = fixture.debugElement.nativeElement;
+      });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SongGenreComponent);
-    component = fixture.componentInstance;
+  it('should have a genre', () => {
+    component.genre = 'Hello World';
     fixture.detectChanges();
+    const genreContainer = compiled.querySelector('[name="genreContainer"]');
+    expect(genreContainer.textContent.trim()).toBe('Hello World');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have a remove container', () => {
+    const genreContainer = compiled.querySelector('.delete');
+    expect(genreContainer.textContent.trim()).toBe('delete');
+  });
+
+  it('should handle remove container click', () => {
+    component.genre = 'Pop';
+
+    const genreContainer = compiled.querySelector('.delete');
+    genreContainer.click();
+
+    component.delete.subscribe($event => {
+      expect($event).toBe('Pop');
+    });
   });
 });
