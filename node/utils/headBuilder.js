2cd58148eebec4829ec2a51838a8d5be52c285bf
Brian Pilati <brianpilati@gmail.com>
2018-08-09 20:18:38 -0600

    Added an html head Builder for the static files

---
+++ node/utils/headBuilder.js
@@ -0,0 +1,10 @@
+module.exports = {
+  buildHead: function(songTitle) {
+    return `
+      <head>
+        <title>Kanikapilas.com : ${songTitle}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      </head>
+    `;
+  }
+};
