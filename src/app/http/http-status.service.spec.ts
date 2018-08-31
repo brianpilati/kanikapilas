2cc92e2f79cb308c4d3de2365f8a4d34607745a7
Brian Pilati <brianpilati@gmail.com>
2018-08-31 11:16:02 -0600

    Added code coverage for http-status.service

--- src/app/http/http-status.service.spec.ts
+++ src/app/http/http-status.service.spec.ts
@@ -2,14 +2,25 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { HttpStatusService } from './http-status.service';
 
-describe('HttpStatusService', () => {
+fdescribe('HttpStatusService', () => {
+  let httpStatusService: HttpStatusService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [HttpStatusService]
     });
   });
 
-  it('should be created', inject([HttpStatusService], (service: HttpStatusService) => {
-    expect(service).toBeTruthy();
+  beforeEach(inject([HttpStatusService], (service: HttpStatusService) => {
+    httpStatusService = service;
   }));
+
+  it('should test get and emit', () => {
+    let emittedStatus = false;
+
+    httpStatusService.getHttpStatusSubject().subscribe(result => (emittedStatus = result));
+
+    httpStatusService.emitHttpStatus(true);
+
+    expect(emittedStatus).toBe(true);
+  });
 });
