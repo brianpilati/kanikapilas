80f0608514f4b4235a0f04aceccaebe9a15cbfde
Brian Pilati <brianpilati@gmail.com>
2018-09-06 11:09:08 -0600

    Fixed artists by search by artistPrefix

--- node/server/domains/artist.js
+++ node/server/domains/artist.js
@@ -1,19 +1,59 @@
 const pool = require('../../lib/database');
 
+function getFindByArtist(letter) {
+  return `(
+    artist LIKE "${letter}%"
+    OR
+    SUBSTRING(artist, LENGTH(artistPrefix) + 2, LENGTH(artist)) LIKE "${letter}%"
+  )`;
+}
+
 module.exports = {
   async getArtistFirstLetter() {
-    return await pool.query(
-      'SELECT SUBSTRING(artist, 1, 1) AS artistFirstLetter FROM songs WHERE active = 1 GROUP BY artistFirstLetter'
-    );
+    return await pool.query(`
+      SELECT
+        SUBSTRING(artist, 1, 1) AS artistFirstLetter
+      FROM
+        songs
+      WHERE
+        active = 1
+      GROUP BY artistFirstLetter
+    `);
   },
 
   async getActiveArtistsByLetter(letter) {
-    return await pool.query(`SELECT * FROM songs WHERE artist LIKE "${letter}%" AND active = 1`);
+    return await pool.query(`
+      SELECT 
+        *
+      FROM 
+        songs
+      WHERE
+        ${getFindByArtist(letter)}
+        AND 
+        active = 1
+      GROUP BY artist
+    `);
   },
 
   async getActiveArtistsCountByLetter(letter) {
-    return await pool.query(
-      `SELECT COUNT(artist) as artist_total FROM songs WHERE artist LIKE "${letter}%" AND active = 1`
-    );
+    return await pool.query(`
+      SELECT 
+        SUM(artists) as artist_total
+      FROM (
+        SELECT
+          COUNT(artist_groups) as artists 
+        FROM (
+          SELECT
+            artist as artist_groups
+          FROM
+            songs
+          WHERE 
+            ${getFindByArtist(letter)}
+            AND active = 1
+          GROUP BY artist
+        ) AS ARTIST_TABLE
+      )
+      AS COUNT_TABLE
+    `);
   }
 };
